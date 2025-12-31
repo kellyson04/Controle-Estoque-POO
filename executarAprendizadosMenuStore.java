42e8be7f18c01a38a7766a021025fab3003422d5
kellyson04 <gilsonvigilante@outlook.com>
2025-12-30 22:08:26 -0300

    Update and rename executarProduto.java to executarAprendizadosMenuStore.java

---
+++ executarAprendizadosMenuStore.java
@@ -0,0 +1,6 @@
+    public class executarAprendizadosMenuStore {
+        public static void main(String[] args) {
+            LoginScreen entrarLoja = new LoginScreen();
+            entrarLoja.accountOptions();
+        }
+    }
--- executarProduto.java
+++
@@ -1,6 +0,0 @@
-public class executarAprendizadosMenuStore {
-    public static void main(String[] args) {
-        StoreSystemMenu menuLoja = new StoreSystemMenu();
-        menuLoja.opcoesMenu();
-    }
-}
