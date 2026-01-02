1a7d6b4b14f77e7088fefe127b3718b05f1fb173
Kellyson Lopes <gilsonvigilante@outlook.com>
2026-01-02 14:15:35 -0300

    refactor: adicionando resiliência ao código e persistência em CSV
    Clean Code: Redução de repetições seguindo o princípio DRY.
    Persistência: Salvamento automático dos dados no arquivo products.csv.
    Integridade: Validação de nomes via Regex para aceitar apenas letras.
    Performance: Otimização das buscas usando break nos laços de repetição.

--- StoreSystemMenu.java
+++ StoreSystemMenu.java
@@ -1,3 +1,6 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,13 +9,13 @@ public class StoreSystemMenu {
     Scanner scanner = new Scanner(System.in);
     int choice = 0;
 
-    //tentando melhorar o programa, porem agora com auxilio de metodos e preferindo switch no lugar de ifs na escolha do menu ja que são numeros fixos
-    public void opcoesMenu() {
+    public void menuOptions() {
         do {
+
+            //se o usuario digitasse uma letra no menu antigamente o programa quebrava, com o metodo readIntNumbers isso não ocorre mais.
             System.out.println("---- Welcome to our STORE CRUD SYSTEM! ----");
-            System.out.print(" 1 - Add Product\n 2 - Remove Product by ID\n 3 - Remove Product by Name\n 4 - Update name,price or quantity\n 5 - List all Products\n 6 - List Products by Category\n 7 - Quit Program \n:");
-            choice = scanner.nextInt();
-            scanner.nextLine();
+            choice = readIntNumbers(" 1 - Add Product\n 2 - Remove Product by ID\n 3 - Remove Product by Name\n 4 - Update name,category,price or quantity\n 5 - List all Products\n 6 - List Products by Category\n 7 - Quit Program \n:");
+
 
             switch (choice) {
                 case 1:
@@ -33,172 +36,177 @@ public class StoreSystemMenu {
                 case 6:
                     listProductsByCategory();
                     break;
+                case 7:
+                    System.out.println("SYSTEM CLOSED . . . BYE");
+                    break;
+                    /*agora com a existencia do default o usuario alem de não conseguir quebrar o programa com o metodo readNumbers digitando letras ao inves de numeros
+                      ele tambem sera avisado quando digitar um numero indisponivel no menu.*/
+                default:
+                    System.out.println("❌THIS OPTION DOESNT EXIST ON MENU❌");
 
             }
         }while (choice != 7 );
     }
     public void addProduct() {
-        System.out.print("Product name:");
-        String productName = scanner.nextLine();
-        System.out.print("Product category:");
-        String productCategory = scanner.nextLine();
-        System.out.print("Product price:");
-        try {
-            double productPrice = scanner.nextDouble();
-            System.out.print("Product quantity:");
-            int productQuantity = scanner.nextInt();
+        //utilizando os metodos como scanner pra evitar quebra do programa ou dados sem sentido ex productName sendo = 1234, oque agora não é mais possivel graças ao regex.
+        String productName = readNames("Product name:");
+        String productCategory = readNames("Product category:");
+        double productPrice = readDoubleNumbers("Product price:");
+        int productQuantity = readIntNumbers("Product quantity:");
+        Produto produto = new Produto(productName,productCategory,productPrice,productQuantity);
+        listaProdutos.add(produto);
+        saveProducts("✅Product successfully added!✅");
 
-            Produto produto = new Produto(productName,productCategory,productPrice,productQuantity);
-            listaProdutos.add(produto);
-            System.out.println("Product successfully added!");
-        }catch (Exception error) {
-            System.err.println("❌ONLY NUMBERS!❌");
-            scanner.nextLine();
-        }
+
     }
 
     public void removeProductById() {
+        System.out.print("AVAILABLE IDS TO REMOVE:");
         for (int i = 0; i < listaProdutos.size(); i++) {
             Produto actualProduct = listaProdutos.get(i);
-            System.out.print("AVAILABLE IDS TO REMOVE:");
             System.out.println(i + " " + actualProduct.getProductName());
 
         }
-        try {
-            System.out.print("Type the ID of the product that you want to remove:");
-            int productToRemove = scanner.nextInt();
-            if (productToRemove >= 0 && productToRemove < listaProdutos.size()) {
-                listaProdutos.remove(productToRemove);
-                System.out.println("Product successfully removed!");
-            }
-        }catch (Exception error) {
-            System.out.println("ONLY NUMBERS!");
+        int productToRemove = readIntNumbers("Enter the ID of the product to remove:");
+        if (productToRemove >= 0 && productToRemove < listaProdutos.size()) {
+            listaProdutos.remove(productToRemove);
+            saveProducts("✅Product successfully removed!✅");
+
+        }else {
+            System.out.println("❌INVALID ID.❌");
         }
     }
 
     public void removeProductByName() {
-        System.out.println("Enter the name of the product to remove:");
-        String productToRemove = scanner.nextLine();
+        listProducts();
 
-        for (int i = 0; i < listaProdutos.size(); i++) {
-            Produto actualProduct = listaProdutos.get(i);
-            if (actualProduct.getProductName().equalsIgnoreCase(productToRemove)) {
-                listaProdutos.remove(actualProduct);
-                System.out.println("Product successfully removed!");
-                break;
+        Produto actualProductName = null;
+
+        while (actualProductName == null) {
+            String productToRemove = readNames("Enter the name that you'd like to remove:");
+            for (Produto eachProductName : listaProdutos) {
+                if (eachProductName.getProductName().equalsIgnoreCase(productToRemove)) {
+                    actualProductName = eachProductName;
+                    break;
+                }
             }
+            if (actualProductName == null) {
+                System.out.println("❌ THIS PRODUCT NAME DOESNT EXIST ❌");
+            }
+
+
         }
+
+        listaProdutos.remove(actualProductName);
+        saveProducts("✅Product successfully removed✅");
+
+
     }
 
-    //em particular o metodo que foi mais dificil de aprender pois tive que entender como funcionam os setters e toda a logica da interação dos loops com o objeto, ainda em aprendizado
     public void updateProduct() {
-        System.out.print("Products Available for Update\n");
-        for (int i = 0; i < listaProdutos.size(); i++) {
-            Produto actualProduct = listaProdutos.get(i);
-            System.out.println(i + " " + actualProduct.getProductName() + " " + actualProduct.getProductCategory() + " " + actualProduct.getProductPrice() + " " + actualProduct.getProductQuantity());
-        }
 
 
-        System.out.println("What would you like to update? 1 - Name | 2 - Category | 3 - Price | 4 - Quantity:");
-        int changeProduct = scanner.nextInt();
-        scanner.nextLine();
+
+        int changeProduct = readIntNumbers("What would you like to update? 1 - Name | 2 - Category | 3 - Price | 4 - Quantity:");
+        System.out.println("Products Available for Update");
+        //agora utilizo o metodo listProducts abaixo pra tentar ao maximo seguir a regra do dont repeat yourself e escrever codigo atoa.
+        listProducts();
 
         if (changeProduct == 1) {
-            System.out.print("Enter the current name of the product:");
-            String productNameToChange = scanner.nextLine();
+            /*primeira vez usando a tecnica de loop com null, utilizei nas outras opções tb, codigo fica muito mais legivel
+              e não tem mais aquele bug chato no qual o usuario digiva um nome ou preco etc que não existia e o programa voltava para o menu
+              sem nenhuma explicação.*/
 
-            System.out.print("Enter the NEW name:");
-            String productNewName = scanner.nextLine();
+            Produto updateName = null;
 
-            for (int i = 0; i < listaProdutos.size(); i++) {
-                Produto actualProduct = listaProdutos.get(i);
 
-                if (actualProduct.getProductName().equalsIgnoreCase(productNameToChange)) {
-                    actualProduct.setProductName(productNewName);
-                    System.out.println("✅Name successfully changed! . . .✅");
-                    break;
+            while (updateName == null) {
+                String productNameToChange = readNames("Enter the current name of the product:");
+
+                for (Produto eachProduct : listaProdutos) {
+                    if (eachProduct.getProductName().equalsIgnoreCase(productNameToChange)) {
+                        updateName = eachProduct;
+                        break;
+                    }
+                }
+
+                if (updateName == null) {
+                    System.err.println("❌ THIS NAME DOESN'T EXIST ❌");
                 }
             }
+
+            String changeProductName = readNames("Enter the new name:");
+            updateName.setProductName(changeProductName);
+            saveProducts("✅Name successfully changed✅");
+
 
         }else if (changeProduct == 2) {
+            Produto actualCategory = null;
 
+            while (actualCategory == null) {
+                String categoryToChange = readNames("Enter the category name:");
+                for (Produto eachProduct : listaProdutos) {
+                    if (eachProduct.getProductCategory().equalsIgnoreCase(categoryToChange)) {
+                        actualCategory = eachProduct;
+                        break;
+                    }
+                }
 
-
-            for (int i = 0; i < listaProdutos.size(); i++) {
-                Produto actualProduct = listaProdutos.get(i);
-                System.out.println("ID " + i + " Category:" +actualProduct.getProductCategory());
-            }
-
-            System.out.println("Enter the product ID:");
-            int idtoChangeCategory = scanner.nextInt();
-            scanner.nextLine();
-
-            for (int i = 0; i < listaProdutos.size(); i++) {
-                Produto actualProduct = listaProdutos.get(i);
-                if (i == idtoChangeCategory) {
-                    System.out.println("Enter the NEW Category:");
-                    String productNewCategory = scanner.nextLine();
-
-                    actualProduct.setProductCategoryName(productNewCategory);
-                    System.out.println("✅Category successfully changed!✅");
-                    break;
+                if (actualCategory == null) {
+                    System.out.println("❌THIS CATEGORY DOESN'T EXIST❌");
                 }
             }
+
+            String updateCategory = readNames("Enter the new category name:");
+            actualCategory.setProductCategoryName(updateCategory);
+            saveProducts("✅Category successfully changed!✅");
+
 
         }else if (changeProduct == 3) {
-            for (int i = 0; i < listaProdutos.size(); i++) {
-                Produto actualProduct = listaProdutos.get(i);
-                System.out.println("ID " + i + " Price $:" +actualProduct.getProductPrice());
-            }
+            Produto actualPrice = null;
 
-            System.out.println("Now Enter the ID:");
-            int idToChangePrice = scanner.nextInt();
-            scanner.nextLine();
+            while (actualPrice == null) {
 
-            for (int i = 0; i < listaProdutos.size(); i++) {
-                Produto actualProduct = listaProdutos.get(i);
-
-                if (i == idToChangePrice) {
-                    System.out.println("Enter the new Price:");
-                    double newProductPrice = scanner.nextDouble();
-                    actualProduct.setProductPrice(newProductPrice);
-                    System.out.println("Product price successfully changed!");
-                    break;
+                int idToChangePrice = readIntNumbers("Enter the id of the product that you'd like to change the price:");
+                if (idToChangePrice >= 0 && idToChangePrice < listaProdutos.size()) {
+                    Produto actualPricee = listaProdutos.get(idToChangePrice);
+                    double newPrice = readDoubleNumbers("Enter the new Price:");
+                    actualPricee.setProductPrice(newPrice);
+                    actualPrice = actualPricee;
+                    saveProducts("✅Price successfully changed!✅");
+                }else {
+                    System.out.println("❌THIS ID IS INVALID❌");
                 }
+
             }
+
+
         }else if (changeProduct == 4) {
-            for (int i = 0; i < listaProdutos.size(); i++) {
-                Produto actualProduct = listaProdutos.get(i);
-                System.out.println("ID " + i + " Quantity:" +actualProduct.getProductQuantity());
-            }
+            Produto actualQuantity = null;
 
-            System.out.println("Now Enter the ID:");
-            int changeQuantityID = scanner.nextInt();
-            scanner.nextLine();
-
-            for (int i = 0;  i < listaProdutos.size(); i++) {
-                Produto actualProduct = listaProdutos.get(i);
-
-                if (i == changeQuantityID) {
-                    System.out.print("Enter the new quantity:");
-                    int newProductQuantity = scanner.nextInt();
-                    actualProduct.setProductQuantity(newProductQuantity);
-                    System.out.println("✅Quantity successfully changed!✅");
-                    break;
+            while (actualQuantity == null) {
+                int idToChangeQuantity = readIntNumbers("Enter the id of the Product that you'd like to change:");
+                if (idToChangeQuantity >= 0 && idToChangeQuantity < listaProdutos.size()) {
+                    actualQuantity = listaProdutos.get(idToChangeQuantity);
+                    int newQuantity = readIntNumbers("Enter the new quantity:");
+                    actualQuantity.setProductQuantity(newQuantity);
+                    saveProducts("✅Quantity successfully changed✅");
+                }else {
+                    System.out.println("❌THIS ID IS INVALID❌");
                 }
             }
+
         }
     }
 
     public void listProducts() {
-        for (Produto eachProductSpecifics : listaProdutos) {
-            System.out.println(eachProductSpecifics);
+        for (int i = 0; i < listaProdutos.size(); i++) {
+            System.out.println("ID " + i + listaProdutos.get(i));
         }
     }
-    
+
     public void listProductsByCategory() {
-        System.out.print("Enter the category you want to list:");
-        String categoryToList = scanner.nextLine();
+        String categoryToList = readNames("Enter category to list:");
 
         for (int i = 0; i < listaProdutos.size(); i++) {
             Produto actualProduct = listaProdutos.get(i);
@@ -209,5 +217,63 @@ public class StoreSystemMenu {
             }
         }
     }
-}
 
+    public String readNames(String message) {
+        while (true) {
+            System.out.println(message);
+            String entrada = scanner.nextLine();
+            if (entrada.matches("[a-zA-Z ]+")) {
+                return entrada;
+            }else {
+                System.err.println("❌ ONLY LETTERS ARE ALLOWED HERE❌");
+            }
+        }
+    }
+
+    public int  readIntNumbers(String message) {
+        while (true) {
+            System.out.println(message);
+
+            String entrada = scanner.nextLine();
+
+            try {
+                int number = Integer.parseInt(entrada);
+                return number;
+
+            }catch (NumberFormatException e) {
+                System.err.println("❌ENTER ONLY NUMBERS PLEASE❌");
+            }
+        }
+    }
+
+    //percebi que com o uso do readIntNumbers no productPrice o usuario não conseguiria colocar por ex 10.50, então resolvi criar o readDoubleNumbers.
+    public double readDoubleNumbers(String message) {
+        while (true) {
+            System.out.println(message);
+
+            String entrada = scanner.nextLine();
+
+            try {
+                double converterEntrada = Double.parseDouble(entrada);
+                return converterEntrada;
+
+            }catch (NumberFormatException e) {
+                System.err.println("❌ENTER ONLY NUMBERS PLEASE❌");
+            }
+        }
+    }
+
+    //mesma função do metodo saveUsers porem sendo utilizado nos produtos, ainda estou tentando entender melhor o funcionamento da biblioteca IO
+    public void saveProducts (String message) {
+        System.out.println(message);
+        try (PrintWriter writer = new PrintWriter(new FileWriter("products.csv"))) {
+
+            for (Produto product : listaProdutos) {
+                writer.println(product.getProductName() + "," + product.getProductCategory() + "," + product.getProductPrice() + "," + product.getProductQuantity());
+            }
+        }catch (IOException error) {
+            System.out.println("❌TECHNICAL ERROR AT THE DISC");
+        }
+    }
+
+}
