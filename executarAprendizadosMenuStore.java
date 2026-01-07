f0b607b1d3e822c5e3bf01c5d034e4f258bf35b0
Kellyson Lopes <gilsonvigilante@outlook.com>
2026-01-07 13:45:09 -0300

    Delete executarAprendizadosMenuStore.java

--- executarAprendizadosMenuStore.java
+++
@@ -1,6 +0,0 @@
-    public class executarAprendizadosMenuStore {
-        public static void main(String[] args) {
-            LoginScreen entrarLoja = new LoginScreen();
-            entrarLoja.accountOptions();
-        }
-    }
